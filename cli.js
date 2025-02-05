4df090a62091e6245cf0649eb00ba99c6172f314
Roman Lamsal <roman@lamsal.de>
2025-02-05 17:37:13 +0100

    feat: allow custom publish args

--- cli.js
+++ cli.js
@@ -14,19 +14,38 @@ if (!packageJsonContents.version) {
 const now = new Date()
 const snapshotTimestamp = [
     now.getFullYear(),
-    now.getMonth() + 1,
-    now.getDate(),
+    (now.getMonth() + 1).toString().padStart(2, "0"),
+    now.getDate().toString().padStart(2, "0"),
     "-",
-    now.getHours(),
-    now.getMinutes(),
-    now.getSeconds(),
+    now.getHours().toString().padStart(2, "0"),
+    now.getMinutes().toString().padStart(2, "0"),
+    now.getSeconds().toString().padStart(2, "0"),
 ].join("")
+
+function getPublishArgs() {
+    const doubleDashIdx = process.argv.findIndex(argv => argv === "--")
+
+    if (doubleDashIdx === -1) {
+        return []
+    }
+
+    const serializedArgs = process.argv.slice(doubleDashIdx + 1, process.argv.length).join(" ")
+
+    const hasTagArg = process.argv.findIndex(argv => argv === "--tag") !== -1
+    if (!hasTagArg) {
+        return `--tag ${snapshotTimestamp} ${serializedArgs}`
+    }
+
+    return serializedArgs
+}
 
 try {
     packageJsonContents.version += `-SNAPSHOT-${snapshotTimestamp}`
     writeFileSync(packageJsonPath, JSON.stringify(packageJsonContents, null, 2))
 
-    execSync(`npm publish --tag ${snapshotTimestamp} --access public`, { stdio: "inherit" })
+    const npmPublishArgs = getPublishArgs()
+
+    execSync(`npm publish ${npmPublishArgs}`, { stdio: "inherit" })
 } finally {
     writeFileSync(packageJsonPath, currentPackageJsonFileContents)
 }
